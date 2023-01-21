920af17a71af4ee27ee5d39f6fb3980b9aacde58
Sayem92 <sayemmiha123@gmail.com>
2023-01-21 13:11:11 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lhckmem.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const usersCollection = client.db("E-commerce-project").collection("users");
+
+
+       
+
+        
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err))
+
+
+app.get('/', async (req, res) => {
+    res.send('react job task server is running');
+})
+
+app.listen(port, () => console.log(`react job task running on ${port}`))
